12f0242e856e399d303a471cf29d12ad4cc35dfe
Poojitha <bandarupoojitha23@gmail.com>
2025-01-28 13:49:15 +0530

    Initial commit with line ending warnings

---
+++ src/main/java/com/ems/entity/ProfessionalDetails.java
@@ -0,0 +1,50 @@
+package com.ems.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "professional_details")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfessionalDetails {
+	  @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long profDetailId;
+
+	    @OneToOne
+	    @JoinColumn(name = "employee_id", referencedColumnName = "employeeId")
+	    private Employee employee;
+
+	    @Column(nullable = false)
+	    private String companyEmail;
+
+	    @Column(nullable = false)
+	    private String officePhone;
+
+	    @Column(nullable = false)
+	    private String officeCity;
+
+	    @Column(nullable = false)
+	    private String officeAddress;
+
+	    @Column(nullable = false)
+	    private String reportingManager;
+
+	    @Column(nullable = false)
+	    private String hrName;
+
+	    @Column(nullable = false)
+	    private String dateOfJoining;
+
+}
---
+++ src/main/java/com/ems/repository/ProfessionalDetailsRepository.java
@@ -0,0 +1,9 @@
+package com.ems.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ems.entity.ProfessionalDetails;
+
+public interface ProfessionalDetailsRepository extends JpaRepository<ProfessionalDetails, Long> {
+
+}
